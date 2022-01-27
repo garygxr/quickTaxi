7873c640043c0813d1044f441f99a879a5443b06
xinrui <ganxinrui123>
2022-01-27 11:39:47 +0800

    文档补充

--- quickTaxi-api/user-api/src/main/java/com/gary/user/dto/request/LoginUser.java
+++ quickTaxi-api/user-api/src/main/java/com/gary/user/dto/request/LoginUser.java
@@ -1,18 +1,23 @@
 package com.gary.user.dto.request;
 
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel
 public class LoginUser {
     /**
      * 账号
      */
+    @ApiModelProperty(value = "用户名",required = true)
     private String username;
 
     /**
      * 密码
      */
+    @ApiModelProperty(value = "密码",required = true)
     private String password;
 
 
